742ac5b857b566af909209240a08074917c7d40b
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2024-03-07 19:06:29 +0100

    init project

--- app.js
+++ app.js
@@ -6,13 +6,38 @@ const expressLayouts = require("express-ejs-layouts");
 const app = express();
 const PORT = 5001 || process.env.PORT;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.get("/", (req, res) => {
+	res.status(200).send({ msg: "Hello" });
+});
 
-// Static Files
-app.use(express.static("public"));
+app.get("/api/users", (req, res) => {
+	res.send([
+		{ id: 1, name: "Khanh1", surname: "Nguyen1" },
+		{ id: 2, name: "Khanh2", surname: "Nguyen2" },
+		{ id: 3, name: "Khanh3", surname: "Nguyen3" },
+		{ id: 4, name: "Khanh4", surname: "Nguyen4" },
+	]);
+});
 
-//Templating Engine
-app.use(expressLayouts);
-app.set("layout", "./layouts/main");
-app.set("view engine", "ejs");
+app.get("/api/product", (req, res) => {
+	res.send([
+		{ id: 1, name: "Product1", surname: "Mark1" },
+		{ id: 2, name: "Product2", surname: "Mark2" },
+		{ id: 3, name: "Product3", surname: "Mark3" },
+		{ id: 4, name: "Product4", surname: "Mark4" },
+	]);
+});
+
+app.listen(PORT, () => {
+	console.log("Running server on " + PORT);
+});
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// // Static Files
+// app.use(express.static("public"));
+
+// //Templating Engine
+// app.use(expressLayouts);
+// app.set("layout", "./layouts/main");
+// app.set("view engine", "ejs");
