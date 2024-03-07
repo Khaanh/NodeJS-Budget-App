5995b5177f5909d80de518c181db1435fd9d6ab4
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2024-03-07 19:35:00 +0100

    feat(views/layuts): added title & description

--- app.js
+++ app.js
@@ -4,40 +4,27 @@ const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 
 const app = express();
-const PORT = 5001 || process.env.PORT;
+const PORT = 5000 || process.env.PORT;
 
-app.get("/", (req, res) => {
-	res.status(200).send({ msg: "Hello" });
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get("/api/users", (req, res) => {
-	res.send([
-		{ id: 1, name: "Khanh1", surname: "Nguyen1" },
-		{ id: 2, name: "Khanh2", surname: "Nguyen2" },
-		{ id: 3, name: "Khanh3", surname: "Nguyen3" },
-		{ id: 4, name: "Khanh4", surname: "Nguyen4" },
-	]);
-});
+// Static Files
+app.use(express.static("public"));
 
-app.get("/api/product", (req, res) => {
-	res.send([
-		{ id: 1, name: "Product1", surname: "Mark1" },
-		{ id: 2, name: "Product2", surname: "Mark2" },
-		{ id: 3, name: "Product3", surname: "Mark3" },
-		{ id: 4, name: "Product4", surname: "Mark4" },
-	]);
+//Templating Engine
+app.use(expressLayouts);
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+	const locals = {
+		title: "NodeJs Notes",
+		description: "Free NodeJs Notes App",
+	};
+	res.render("index", locals);
 });
 
 app.listen(PORT, () => {
-	console.log("Running server on " + PORT);
+	console.log("App listening on port: " + PORT);
 });
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// // Static Files
-// app.use(express.static("public"));
-
-// //Templating Engine
-// app.use(expressLayouts);
-// app.set("layout", "./layouts/main");
-// app.set("view engine", "ejs");
