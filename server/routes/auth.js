415ff03c8ae2aebcbcefe1137eabf900b8b2d70a
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2024-03-10 20:49:46 +0100

    feat(auth): caught some trouble with mongoose

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("../models/User");
 
 passport.use(
 	new GoogleStrategy(
@@ -10,8 +11,27 @@ passport.use(
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 			callbackURL: process.env.GOOGLE_CALLBACK_URL,
 		},
-		function (accessToken, refreshToken, profile, cb) {
-			console.log(profile);
+		async function (accessToken, refreshToken, profile, done) {
+			const newUser = {
+				googleId: profile.id,
+				displayName: profile.displayName,
+				firstName: profile.name.givenName,
+				lastName: profile.name.familyName,
+				profileImage: profile.photos[0].value,
+			};
+
+			try {
+				let user = await User.findOne({ googleId: profile.id });
+
+				if (user) {
+					done(null, user);
+				} else {
+					user = await User.create(newUser);
+					done(null, user);
+				}
+			} catch (error) {
+				console.log(error);
+			}
 		}
 	)
 );
@@ -33,7 +53,19 @@ router.get(
 
 // Route if something goes wrong
 router.get("/login-failure", (req, res) => {
-	res.send("Someting went wrong...");
+	res.send("Something went wrong...");
+});
+
+// Destroy user session
+router.get("/logout", (req, res) => {
+	req.session.destroy((error) => {
+		if (error) {
+			console.log(error);
+			res.send("Error login out");
+		} else {
+			res.redirect("/");
+		}
+	});
 });
 
 // Persist user data after successful authentication
@@ -43,7 +75,7 @@ passport.serializeUser(function (user, done) {
 
 // Retrieve user data from session
 passport.deserializeUser(function (id, done) {
-	User.FindById(id, function (err, user) {
+	User.findById(id, function (err, user) {
 		done(err, user);
 	});
 });
