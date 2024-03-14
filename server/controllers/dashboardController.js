db0152e16356eb9a84914128a5e6bbc526edd289
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2024-03-14 22:12:35 +0100

    Done

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -1,12 +1,10 @@
 const Note = require("../models/Notes");
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
 
 /**
  * GET /
  * Dashboard
  */
-
 exports.dashboard = async (req, res) => {
 	let perPage = 12;
 	let page = req.query.page || 1;
@@ -20,7 +18,7 @@ exports.dashboard = async (req, res) => {
 		// Mongoose "^7.0.0 Update
 		const notes = await Note.aggregate([
 			{ $sort: { updatedAt: -1 } },
-			{ $match: { user: new mongoose.Types.ObjectId(req.user.id) } },
+			{ $match: { user: mongoose.Types.ObjectId(req.user.id) } },
 			{
 				$project: {
 					title: { $substr: ["$title", 0, 30] },
@@ -42,6 +40,33 @@ exports.dashboard = async (req, res) => {
 			current: page,
 			pages: Math.ceil(count / perPage),
 		});
+
+		// Original Code
+		// Note.aggregate([
+		//   { $sort: { updatedAt: -1 } },
+		//   { $match: { user: mongoose.Types.ObjectId(req.user.id) } },
+		//   {
+		//     $project: {
+		//       title: { $substr: ["$title", 0, 30] },
+		//       body: { $substr: ["$body", 0, 100] },
+		//     },
+		//   },
+		// ])
+		//   .skip(perPage * page - perPage)
+		//   .limit(perPage)
+		//   .exec(function (err, notes) {
+		//     Note.count().exec(function (err, count) {
+		//       if (err) return next(err);
+		//       res.render("dashboard/index", {
+		//         userName: req.user.firstName,
+		//         locals,
+		//         notes,
+		//         layout: "../views/layouts/dashboard",
+		//         current: page,
+		//         pages: Math.ceil(count / perPage),
+		//       });
+		//     });
+		//   });
 	} catch (error) {
 		console.log(error);
 	}
@@ -51,14 +76,13 @@ exports.dashboard = async (req, res) => {
  * GET /
  * View Specific Note
  */
-
 exports.dashboardViewNote = async (req, res) => {
 	const note = await Note.findById({ _id: req.params.id })
 		.where({ user: req.user.id })
 		.lean();
 
 	if (note) {
-		res.render("dashboard/view-note", {
+		res.render("dashboard/view-notes", {
 			noteID: req.params.id,
 			note,
 			layout: "../views/layouts/dashboard",
@@ -68,4 +92,131 @@ exports.dashboardViewNote = async (req, res) => {
 	}
 };
 
-exports.dashboardUpdateNote = async (req, res) => {};
+/**
+ * PUT /
+ * Update Specific Note
+ */
+exports.dashboardUpdateNote = async (req, res) => {
+	try {
+		await Note.findOneAndUpdate(
+			{ _id: req.params.id },
+			{ title: req.body.title, body: req.body.body, updatedAt: Date.now() }
+		).where({ user: req.user.id });
+		res.redirect("/dashboard");
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+/**
+ * DELETE /
+ * Delete Note
+ */
+exports.dashboardDeleteNote = async (req, res) => {
+	try {
+		await Note.deleteOne({ _id: req.params.id }).where({ user: req.user.id });
+		res.redirect("/dashboard");
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+/**
+ * GET /
+ * Add Notes
+ */
+exports.dashboardAddNote = async (req, res) => {
+	res.render("dashboard/add", {
+		layout: "../views/layouts/dashboard",
+	});
+};
+
+/**
+ * POST /
+ * Add Notes
+ */
+exports.dashboardAddNoteSubmit = async (req, res) => {
+	try {
+		req.body.user = req.user.id;
+		await Note.create(req.body);
+		res.redirect("/dashboard");
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+/**
+ * GET /
+ * Search
+ */
+exports.dashboardSearch = async (req, res) => {
+	try {
+		res.render("dashboard/search", {
+			searchResults: "",
+			layout: "../views/layouts/dashboard",
+		});
+	} catch (error) {}
+};
+
+/**
+ * POST /
+ * Search For Notes
+ */
+exports.dashboardSearchSubmit = async (req, res) => {
+	try {
+		let searchTerm = req.body.searchTerm;
+		const searchNoSpecialChars = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "");
+
+		const searchResults = await Note.find({
+			$or: [
+				{ title: { $regex: new RegExp(searchNoSpecialChars, "i") } },
+				{ body: { $regex: new RegExp(searchNoSpecialChars, "i") } },
+			],
+		}).where({ user: req.user.id });
+
+		res.render("dashboard/search", {
+			searchResults,
+			layout: "../views/layouts/dashboard",
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+/**
+ * GET /
+ * Search For Notes
+ */
+
+exports.dashboardSearch = async (req, res) => {
+	try {
+		res.render("dashboard/search", {
+			searchResults: "",
+			layout: "../views/layout/dashboard",
+		});
+	} catch (error) {}
+};
+
+/**
+ * PSOT /
+ * Search For Notes
+ */
+
+exports.dashboardSearchSubmit = async (req, res) => {
+	try {
+		let searchTerm = req.body.searchTerm;
+		const searchNoSpecialChars = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "");
+
+		const searchResults = await Note.find({
+			$or: [
+				{ title: { $regex: new RegExpI(searchNoSpecialChars, "i") } },
+				{ body: { $regex: new RegExpI(searchNoSpecialChars, "i") } },
+			],
+		}).where({ user: req.user.id });
+
+		res.render("dashboard/search", {
+			searchResults,
+			layout: "../views/layout/dashboard",
+		});
+	} catch (error) {}
+};
